a444885a556694196177865d652cd6a4b4b29eb4
sharonasaja <sharon.asaja@student.moringaschool>
2023-09-27 00:35:25 +0300

    attempted lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
+
 def write_file(file_name, file_content):
-    pass
+    with open(file_name, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(file_name, 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(file_name, 'r') as file:
+        content = file.read()
+        return content
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added")
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+print(read_file(file_name="logfile"))
